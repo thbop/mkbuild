8474b978a6bff656ebd432803eab2e2c0defe04f
Thbop <thbopcoder@gmail.com>
2026-07-25 18:55:02 -0400

    wip: Add preprocess to GNU C

--- src/mkbuild/compilers/gnu_c.py
+++ src/mkbuild/compilers/gnu_c.py
@@ -1,5 +1,16 @@
-from ..compiler import Compiler
+from mkbuild.compiler import Compiler
+from mkbuild.utils import run_silent
 
 
 class GNU_C(Compiler):
     """The GNU C Compiler implementation."""
+
+    @property
+    def compiler(self) -> str:
+        """GCC Compiler."""
+        return "gcc"
+
+    def preprocess(self, filename) -> str:
+        """Preprocess for GCC."""
+        result = run_silent(self.compiler, "-E", self.flags, filename)
+        return result.stdout
