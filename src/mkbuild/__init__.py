1cd3df0e58dcacd783368f7169ff039df32f0d2d
Thbop <thbopcoder@gmail.com>
2026-07-25 22:29:09 -0400

    chore: Ensure bin directory and module stuff

--- src/mkbuild/__init__.py
+++ src/mkbuild/__init__.py
@@ -1,4 +1,22 @@
 import os
 
+from .compiler import Compiler
+from .context import Context
+from .flags import Flags, OSFlags
+from .linker import Linker
+from .sources import Sources
+from .utils import run, run_silent
+
 env = os.environ.copy()
 env["FORCE_COLOR"] = "1"
+
+__all__ = [
+    "Compiler",
+    "Linker",
+    "Context",
+    "Flags",
+    "OSFlags",
+    "Sources",
+    "run",
+    "run_silent",
+]
--- src/mkbuild/context.py
+++ src/mkbuild/context.py
@@ -1,10 +1,14 @@
 import sys
 from dataclasses import dataclass
+from pathlib import Path
 
 
 @dataclass(frozen=True)
 class Context:
     """Context object to keep track of paths and state."""
+
+    def __post_init__(self) -> None:  # noqa: D105
+        Path(self.BIN_PATH).mkdir(parents=True, exist_ok=True)
 
     SRC_PATH: str = "src"
     BIN_PATH: str = "bin"
